86cbea21c08d3e21cf11fd4033eee45ef7fa7ec8
jane <eishwaryatiwari0604@gmail.com>
2024-09-04 13:02:53 +0530

    work compplete

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import "./App.css";
+import CountriesTables from "./components/CountriesTables";
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="d-flex flex-column align-items-center">
+        <h1>Countries DataTable</h1>
+      </div>
+      <CountriesTables />
+    </>
+  );
+}
+
+export default App;
